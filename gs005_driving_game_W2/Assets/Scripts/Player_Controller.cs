79249e52966ced9c613987e399f6b1a3d205bc7f
ApolloGOM <ApolloGOM365@gmail.com>
2025-05-15 17:04:34 +0100

    Updates to code
    Updates to code including a player 2 side of things. Changing the control scheme's in the inputs so that two sides of the keyboard can work on the same game. Changing camera angles so that the game now has split screen. And also making it so the car's only move when pressing forward instead of just a constant speed.

--- gs005_driving_game_W2/Assets/Scripts/Player_Controller.cs
+++ gs005_driving_game_W2/Assets/Scripts/Player_Controller.cs
@@ -4,6 +4,11 @@ public class Player_Controller : MonoBehaviour
 {
     public float speed = 6;
     private Rigidbody rb;
+    float horizontalInput, verticalInput;
+    public float turnSpeed;
+
+    public int playerIndex; //if this value is 1, it will be player 1. If this value is set as 2, it will be player 2.
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,13 +22,27 @@ public class Player_Controller : MonoBehaviour
         //transform.Translate(0,0,speed * Time.deltatime);
 
         //Method 2, using translation for movement but while using short forms
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         //Method 3, using the physics engine to move our object forward
         //rb.AddForce(Vector3.forward * speed);
 
         //Method 4, using the physics engine to move our object forward
         //rb.AddRelativeForce(Vector3.forward * speed);
+
+        //Movement mechanics
+
+        //Recording Player Inputs
+
+        horizontalInput = Input.GetAxis("Horizontal" + playerIndex); //Left, Right, A, D, Analogue Stick Left/Right
+        verticalInput = Input.GetAxis("Vertical" + playerIndex); //Up, Drown, W, S, Analogue Stick Up/Down
+
+        //Move the player left and right
+        
+        //If you change Translate to rotate and Vector3.Right to up it will turn instead of shift
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
     }
 
